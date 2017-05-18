02b012021534c07d8d078c3cfafe5f36443304e8
15017082 <15017082@myrp.edu.sg>
2017-05-18 13:23:13 +0800

    Juble

--- app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/MainActivity.java
@@ -1,13 +1,66 @@
 package com.myapplicationdev.android.p04_revisionnotes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edNote;
+    RadioGroup rgStars;
+    Button btnInsert, btnShow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edNote = (EditText)findViewById(R.id.editTextNote);
+        rgStars = (RadioGroup)findViewById(R.id.radioGroupStars);
+        btnInsert = (Button)findViewById(R.id.buttonInsertNote);
+        btnShow = (Button)findViewById(R.id.buttonShowList);
+
+        btnInsert.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+               //getting the selected radio button's text from the radio group
+                int radioButtonID = rgStars.getCheckedRadioButtonId();
+                RadioButton radioButton = (RadioButton) rgStars.findViewById(radioButtonID);
+                String selectedStar = (String) radioButton.getText();
+                Integer star = Integer.parseInt(selectedStar);
+
+                //getting data from the edit text field
+                String note =  edNote.getText().toString();
+
+                // Create the DBHelper object, passing in the
+                // activity's Context
+                DBHelper db = new DBHelper(MainActivity.this);
+
+                // Insert a task
+                db.insertNote(note, star);
+                db.close();
+
+                Toast.makeText(MainActivity.this, "Inserted!",
+                        Toast.LENGTH_LONG).show();
+            }
+
+        });
+
+        btnShow.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/RevisionNotesArrayAdapter.java
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/RevisionNotesArrayAdapter.java
@@ -32,14 +32,38 @@ public class RevisionNotesArrayAdapter extends ArrayAdapter<Note> {
 
 		//Match the UI components with Java variables
 
+		TextView tvContent = (TextView) rowView.findViewById(R.id.textViewNote);
+		 iv1 = (ImageView) rowView.findViewById(R.id.imageView1star);
+		 iv2 = (ImageView) rowView.findViewById(R.id.imageView2star);
+		 iv3 = (ImageView) rowView.findViewById(R.id.imageView3star);
+		 iv4 = (ImageView) rowView.findViewById(R.id.imageView4star);
+		 iv5 = (ImageView) rowView.findViewById(R.id.imageView5star);
+
 		Note note = notes.get(position);
+		String content = note.getNoteContent();
+		tvContent.setText(content);
+		int stars = note.getStars();
 
 		//Check if the property for starts >= 5, if so, "light" up the stars
-		if (/*stars >= 5*/) {
+		if (stars >= 5) {
 			iv5.setImageResource(android.R.drawable.btn_star_big_on);
 			iv4.setImageResource(android.R.drawable.btn_star_big_on);
 			iv3.setImageResource(android.R.drawable.btn_star_big_on);
 			iv2.setImageResource(android.R.drawable.btn_star_big_on);
+			iv1.setImageResource(android.R.drawable.btn_star_big_on);
+		} else if (stars == 4){
+			iv4.setImageResource(android.R.drawable.btn_star_big_on);
+			iv3.setImageResource(android.R.drawable.btn_star_big_on);
+			iv2.setImageResource(android.R.drawable.btn_star_big_on);
+			iv1.setImageResource(android.R.drawable.btn_star_big_on);
+		} else if (stars == 3){
+			iv3.setImageResource(android.R.drawable.btn_star_big_on);
+			iv2.setImageResource(android.R.drawable.btn_star_big_on);
+			iv1.setImageResource(android.R.drawable.btn_star_big_on);
+		} else if (stars == 2){
+			iv2.setImageResource(android.R.drawable.btn_star_big_on);
+			iv1.setImageResource(android.R.drawable.btn_star_big_on);
+		} else if (stars == 1){
 			iv1.setImageResource(android.R.drawable.btn_star_big_on);
 		}
 
